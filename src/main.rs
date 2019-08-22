9d41667c81a9cb35ae27fd79f9e2efcee9844b66
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2019-08-22 09:33:23 -0700

    read the docs!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world from Portland");
+}
+fn main() {
+    println!("Read the Docs at doc.rust-lang.org");
+    println!("or");
+    println!("rustup doc --std");
 }
