18bd772465c3247a07df47776661462badf1fa77
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2019-08-22 10:06:28 -0700

    playing with Rust types

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,20 @@ fn main() {
     println!("rustup doc --std");
 
     my_arrays();
+    who_am_i();
 }
 
 fn who_am_i(){
 
     let name = "Jami";
-    let mut age: 1;
-    
+    let mut age= 1;
+    for i in 0..50 {
+        age += i;
+        if age > 1250 {
+             panic!("aaaaa!");
+        }
+    }
+    age = add_fifty(age);
     println!("Hi, {}! You are {} years old.", name,  age);
 }
 
@@ -20,21 +27,19 @@ fn add_fifty(n: i32) -> i32 {
     n + 50
 }
 
-fn roller_coaster(){
-    let height = 167u32;
-    match height {
-        0...150 => println!("You're too small to go on the rollercoaster."),
-        150...200 => println!("You may go on the rollercoaster!"),
-        _ => {
-            println!("You're too tall to go on the rollercoaster.");
-        },
-    }
-}
+
 
 fn my_arrays(){
     let mut color = [255, 0, 255];
     color[0] = 100;
     println!("The color is {:?}", color);
     println!("Normally use Vectors");
-    println!("Pretty print {:#}, color")
+    println!("Pretty print {:#?}", color);
+
+    let mut prices = vec![30, 100, 2];
+    prices[0] = 25;
+    prices.push(40);
+    println!("All the prices are: {:#?}", prices);
+
+
 }
