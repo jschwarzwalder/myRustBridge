a8656a06206d056724f7856230af8a8d3d3eb2b4
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2019-08-22 09:45:07 -0700

    making a function

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 fn main() {
-    println!("Hello, world from Portland");
-}
-fn main() {
+//    println!("Hello, world from Portland");
+
     println!("Read the Docs at doc.rust-lang.org");
     println!("or");
     println!("rustup doc --std");
+
+
+    let name = "Jami";
+    let age: i32 = 50;
+    println!("Hi, {}! You are {} years old.", name,  add_fifty(age));
+}
+
+fn add_fifty(n: i32) -> i32 {
+    n + 50
 }
